2924001bf51d675ee5136514a7a7392e0f9f0044
Mauro Moura <mauro@loyall.com.br>
2022-05-07 12:06:22 -0300

    color keyboard and animated letters

--- js/game.js
+++ js/game.js
@@ -15,17 +15,41 @@ Super difícil: não pintar o teclado e não deixar mais tentativas
 Ou melhor: fazer uma configuração para o nível de dificuldade
 */
 
-const animateResults = (comparisonResults, triedDiacritic) => {
+const wait = async (time) => {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+const animateResults = async (comparisonResults, triedDiacritic) => {
     const firstIndex = (triedWords.length - 1) * 5
     for (var i = firstIndex; i < firstIndex + 5; i++) {
         const squareEl = document.getElementById(i);
         squareEl.classList.add("animate__flipInX");
         squareEl.style.backgroundColor = colors[comparisonResults[i - firstIndex]];
+        squareEl.innerHTML = triedDiacritic[i - firstIndex];
+        await wait(75);
     }
 }
 
-const processAnswer = () => {
+const colorKeyboard = (comparisonResults, triedWord) => {
+    // We need to priorize the correct ones
+    triedWord.split('').forEach((letter, index) => {
+        const key = document.querySelector(`[data-key="${letter}"]`);
+        const keyColor = key.style.backgroundColor
+        if (keyColor === colors.correct) return
+        if (comparisonResults[index] === 'correct') {
+            key.style.backgroundColor = colors.correct;
+            return
+        }
+        if (comparisonResults[index] === 'misplaced') {
+            key.style.backgroundColor = colors.misplaced;
+            return
+        }
+        if (keyColor === colors.misplaced) return
+        key.style.backgroundColor = colors.wrong;
+    })
+}
 
+const processAnswer = () => {
     const triedWord = triedWords[triedWords.length - 1];
     const triedDiacritic = words.getDiacriticWord(triedWord);
     if (!triedDiacritic) {
@@ -35,6 +59,7 @@ const processAnswer = () => {
 
     const comparisonResults = words.getLettersComparison(triedWord, word)
     animateResults(comparisonResults, triedDiacritic);
+    colorKeyboard(comparisonResults, triedWord);
     if (triedWord === word) {
         alert('You won!')
     }
@@ -44,24 +69,25 @@ const processAnswer = () => {
     triedWords.push('')
 }
 
-const updateWords = () => {
-    const lettersList = triedWords.join('').split('')
-    const squares = document.querySelectorAll('.square');
-    squares.forEach((square, index) => {
-        square.innerHTML = lettersList[index] || '';
-    })
+const updateLastWord = () => {
+    const lastWord = triedWords[triedWords.length - 1]
+    const firstIndex = (triedWords.length - 1) * 5
+    for (var i = firstIndex; i < firstIndex + 5; i++) {
+        const squareEl = document.getElementById(i);
+        squareEl.innerHTML = lastWord[i - firstIndex] || '';
+    }
 }
 
 const insertLetter = (letter) => {
     if (triedWords[triedWords.length - 1].length >= 5) return
     triedWords[triedWords.length - 1] = triedWords[triedWords.length - 1].concat(letter);
-    updateWords()
+    updateLastWord()
 }
 
 const removeLastLetter = () => {
     if (triedWords[triedWords.length - 1].length === 0) return
     triedWords[triedWords.length - 1] = triedWords[triedWords.length - 1].slice(0, -1);
-    updateWords()
+    updateLastWord()
 }
 
 const keyListener = (keyElement) => {
